bdebe6ab39cad801a8c9d01f9c31f2a062ed86a4
tratakatea <tratakatea@gmail.com>
2016-07-21 20:17:33 +0300

    second commit add site

---
+++ js/scriptload.js
@@ -0,0 +1,7 @@
+//Прелоадер для сайта
+$(window).on('load', function () {
+	    	var $preloader = $('#page-preloader'),
+	    	$spinner   = $preloader.find('.spinner');
+	    	$spinner.fadeOut();
+	    	$preloader.delay(350).fadeOut('slow');
+	    });
